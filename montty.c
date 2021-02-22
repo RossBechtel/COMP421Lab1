a5514bf8ceb13d31ea81f77dbb378ab1bfc8d791
Ross Ashok Bechtel <rab17@amber.clear.rice.edu>
2021-02-22 17:18:07 -0600

    init hardware

--- montty.c
+++ montty.c
@@ -2,20 +2,28 @@
 #include <terminals.h>
 #include <hardware.h>
 
-void WriteDataRegister(int term, char c) {
-    (void)term;
-    (void)c;
-}
 
-char ReadDataRegister(int term) {
-    (void)term;
-    return('0');
-}
 
-int InitHardware(int term) {
-    (void)term;
-    return(0);
-}
+/**
+Provided:
+hardware.h
+
+void WriteDataRegister(int term, char c) 
+This hardware operation places the character c in the output data register of the terminal identified
+by the terminal number term. On any error, in this project, this function prints an error message on
+stderr and terminates the program.
+
+char ReadDataRegister(int term)
+This hardware operation reads (and returns) the current contents of the input data register of the
+terminal identified by term. On any error, in this project, this function prints an error message on
+stderr and terminates the program.
+
+int InitHardware(int term)
+This hardware operation initializes the terminal identified by term. It must be called once and only
+once before calling any of the other hardware procedures on that terminal. Returns 0 on success
+or -1 on any error.
+
+**/
 
 void ReceiveInterrupt(int term) {
     (void)term;
@@ -26,10 +34,11 @@ void TransmitInterrupt(int term) {
 }
 
 int WriteTerminal(int term, char *buf, int buflen) {
+    if(buflen == 0)
+        return(0);
     (void)term;
     (void)buf;
-    (void)buflen;
-    return(0);
+    return(buflen);
 }
 
 int ReadTerminal(int term, char *buf, int buflen) {
@@ -38,10 +47,13 @@ int ReadTerminal(int term, char *buf, int buflen) {
     (void)buflen;
     return(0);
 }
-
+/**
+ * This procedure should be called once and only once before any other call to the terminal device driver procedures defined above are called for terminal term.
+ * Dont create threads in here
+**/
 int InitTerminal(int term) {
-    (void)term;
-    return(0);
+    Declare_Monitor_Entry_Procedure();
+    return(InitHardware(term));
 }
 
 int TerminalDriverStatistics(struct termstat *stats) {
@@ -50,11 +62,5 @@ int TerminalDriverStatistics(struct termstat *stats) {
 }
 
 int InitTerminalDriver() {
-    return(0);
-}
-
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
     return(0);
 }
